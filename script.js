81cd595b0613f62b3b94e3ded46e6faa2f472e60
mv2y <gfihrf44@gmail.com>
2024-10-08 18:29:44 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-// التحقق من إرسال نموذج الاتصال
+// Initialize EmailJS with your User ID
+(function() {
+    emailjs.init("QiUwjAh2wGdXwfV7y"); // استبدل YOUR_USER_ID بمعرف المستخدم الخاص بك
+})();
+
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    // بيانات النموذج
-    const templateParams = {
+    // Send email using EmailJS
+    emailjs.send('service_nb5trg7', 'service_nb5trg7', {
         from_name: document.getElementById('name').value,
         email_id: document.getElementById('email').value,
-        location: document.getElementById('location').value,
         message: document.getElementById('message').value
-    };
-
-    // استخدام EmailJS لإرسال الرسالة
-    emailjs.send('service_nb5trg7', 'template_xgnmtbg', templateParams)
-        .then(function(response) {
-           alert('تم إرسال الرسالة بنجاح!');
-        }, function(error) {
-           alert('حدث خطأ أثناء الإرسال. حاول مرة أخرى.');
-        });
+    }).then(function(response) {
+        console.log('SUCCESS!', response.status, response.text);
+        alert('تم إرسال الرسالة بنجاح!');
+    }, function(error) {
+        console.log('FAILED...', error);
+        alert('حدث خطأ أثناء الإرسال...');
+    });
 });
